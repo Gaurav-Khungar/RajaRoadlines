504a74b0dbc624fcbb364af00429ca6961623d13
Gaurav Khungar <gauravkhungar97@gmail.com>
2026-04-08 02:52:55 +0530

    logo fixed

--- js/logo.js
+++ js/logo.js
@@ -4,7 +4,7 @@
 document.querySelectorAll('.logo-mark').forEach(el => {
   const size = parseInt(el.dataset.size || 36);
   const img = document.createElement('img');
-  img.src = 'images/logo.svg';
+  img.src = 'images/logo.png';
   img.alt = 'Raja Roadlines';
   img.style.height = size + 'px';
   img.style.width = 'auto';
